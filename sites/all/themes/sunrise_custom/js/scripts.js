75a7f31c2e12ecacecf5062791c6ef8b67c046e0
Jason Awbrey and Jordan Meier <jason.s.awbrey@gmail.com>
2016-05-05 10:30:33 -0700

    add jquery to change color of buttons to light blue when hovering over them

--- sites/all/themes/sunrise_custom/js/scripts.js
+++ sites/all/themes/sunrise_custom/js/scripts.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($) {
-    $('#js-button').click(function(){
+    $('#js-button, #js-button2').click(function(){
         alert('BANANA SHAAAAARK!');
     });
+
+    $('#js-button2, #js-button').hover(function(){
+        $(this).addClass("hover");
+    }, function(){
+        $(this).removeClass("hover");
+    }
+    );
 });
